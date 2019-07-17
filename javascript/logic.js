96feb1996266a7e708eea78548bedd893237611f
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-17 00:09:27 -0500

    working on js

--- javascript/logic.js
+++ javascript/logic.js
@@ -30,3 +30,32 @@ var options = [
         photo: ""
     }
 ];
+
+//variables
+var timeCounter = 20;
+var currentQuestion = 0;
+var correct = 0;
+var wrong = 0;
+var timer;
+
+function displayquestion (){
+    var questions =options[currentQuestion].question;
+    var choices =options[currentQuestion].choice;
+
+    $("#time").html("Timer: " + timeCounter)
+    $("#game").html(
+        `<h4>${questions}</h4>
+        ${displayChoices(choices)}
+        
+    `);
+}
+displayquestion();
+
+function displayChoices (choice){
+    
+    var result = ""
+    for (var i = 0; i <choices.result; i++ ){
+        result += `<p class="choice" data-answer ="${choice[i]}">"${choice[i]}"</p>`
+    }
+    return result;
+}
