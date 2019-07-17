0578d4e14f6030787ac46ada4aeb32ba8bc8ccaf
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-17 16:41:00 -0500

    transforming the js to work property, displaying question, ans, and result page

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,61 +1,135 @@
-var options = [
+var questions = [
     {
         question:"which of these countries belong to the African continent",
-        choice: ["Ghana","Fiji","Cuba","Peru"],
-        answer: 0,
-        photo: ""
+        choices: ["Ghana","Fiji","Cuba","Peru"],
+        answer: "Ghana",
+        rightPhoto: "",
+        wrongPhoto: ""
     },
     {
         question:"Which African Country has the biggest Land Area",
-        choice: ["Sudan","Nigeria","Mali","Algeria"],
-        answer: 4,
-        photo: ""
+        choices: ["Sudan","Nigeria","Mali","Algeria"],
+        answer: "Algeria",
+        rightPhoto: "",
+        wrongPhoto: ""
     },
     {
         question:"which of these countries belong to the African continent",
-        choice: ["Ghana","Fiji","Cuba","Peru"],
-        answer: 0,
-        photo: ""
+        choices: ["Ghana","Fiji","Cuba","Peru"],
+        answer: "Ghana",
+        rightPhoto: "",
+        wrongPhoto: ""
     },
     {
         question:"Which African Country has the biggest Land Area",
-        choice: ["Sudan","Nigeria","Mali","Algeria"],
-        answer: 4,
-        photo: ""
+        choices: ["Sudan","Nigeria","Mali","Algeria"],
+        answer: "Algeria",
+        rightPhoto: "",
+        wrongPhoto: ""
     },
     {
         question:"which of these countries belong to the African continent",
-        choice: ["Ghana","Fiji","Cuba","Peru"],
-        answer: 0,
-        photo: ""
+        choices: ["Ghana","Fiji","Cuba","Peru"],
+        answer: "Ghana",
+        rightPhoto: "",
+        wrongPhoto: ""
     }
 ];
 
-//variables
-var timeCounter = 20;
-var currentQuestion = 0;
-var correct = 0;
-var wrong = 0;
-var timer;
 
-function displayquestion (){
-    var questions =options[currentQuestion].question;
-    var choices =options[currentQuestion].choice;
+var index = 0
+var rightAnswers = 0;
+var wrongAnswers =0;
+var timer = 20;
 
-    $("#time").html("Timer: " + timeCounter)
-    $("#game").html(
-        `<h4>${questions}</h4>
-        ${displayChoices(choices)}
-        
-    `);
-}
-displayquestion();
 
-function displayChoices (choice){
+// directly bound event
+$('#start').on('click', function(){
+    rightAnswers = 0
+    wrongAnswers = 0
+    displayNextQuestion();
     
-    var result = ""
-    for (var i = 0; i <choices.result; i++ ){
-        result += `<p class="choice" data-answer ="${choice[i]}">"${choice[i]}"</p>`
+    // hide start button
+    $('#start').hide();
+
+    //start timer
+
+})
+
+
+function displayNextQuestion(){
+    // show first question
+    $('#question').html(questions[index].question)
+
+
+    // empty choice div
+    $('#choices').empty();
+    // $('#response').empty();
+
+
+    // show first question choices
+    for (let i = 0; i < questions[index].choices.length; i++) {
+        $('#choices').append(`<button class="choice">${questions[index].choices[i]}</button>`)    
     }
-    return result;
 }
+
+
+// click event for each choice
+// delegated event
+$('body').on('click','.choice', function(){
+    var selectedChoice = $(this).text();
+    console.log(selectedChoice);
+    console.log(questions[index].answer);
+    // check if right or wrong
+    if(selectedChoice === questions[index].answer){
+        alert('you are right');
+        //$("#response").html('<h2>you are right</h2>');
+        // if right increment rightAnswers
+        rightAnswers ++;
+        
+
+    } else {
+        alert('you are wrong')
+        // if wrong decrement wrongAnswers
+        wrongAnswers ++;
+    };
+
+    index++;
+
+    if(index < 5){
+        displayNextQuestion();
+    } else {
+        // game over
+        alert('game over')
+        // hide questions and choices
+        // show end results
+        $("#score").append(`CORRECT ANSWERS :<p>${rightAnswers}</p>`);
+        $("#score").append(`WRONG ANSWERS :<p>${wrongAnswers}</p>`);
+        // rightAnswers = 0
+        // wrongAnswers = 0
+        // show start
+        //$('#start').show();
+
+        //hide questions
+        $('#question').hide();
+        $('#choices').hide();
+        
+        //hide choices
+    };
+    
+})
+
+// move to next question
+
+// get through all questions
+// show result - right wrong
+
+// track wins and losses
+
+//reset
+
+
+// NICE EXTRA FEATURES
+
+// timer
+// giphy for right wrongs
