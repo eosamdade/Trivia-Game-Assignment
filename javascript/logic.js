e7e50a3542bb8bc1dc9d29ce879dae2648789657
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-16 23:42:49 -0500

    worked on uploading necessary files and worked on html

---
+++ javascript/logic.js
@@ -0,0 +1,32 @@
+var options = [
+    {
+        question:"which of these countries belong to the African continent",
+        choice: ["Ghana","Fiji","Cuba","Peru"],
+        answer: 0,
+        photo: ""
+    },
+    {
+        question:"Which African Country has the biggest Land Area",
+        choice: ["Sudan","Nigeria","Mali","Algeria"],
+        answer: 4,
+        photo: ""
+    },
+    {
+        question:"which of these countries belong to the African continent",
+        choice: ["Ghana","Fiji","Cuba","Peru"],
+        answer: 0,
+        photo: ""
+    },
+    {
+        question:"Which African Country has the biggest Land Area",
+        choice: ["Sudan","Nigeria","Mali","Algeria"],
+        answer: 4,
+        photo: ""
+    },
+    {
+        question:"which of these countries belong to the African continent",
+        choice: ["Ghana","Fiji","Cuba","Peru"],
+        answer: 0,
+        photo: ""
+    }
+];
